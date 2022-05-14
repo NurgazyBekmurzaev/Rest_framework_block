e975e82716d07bf0253453bcc2a118aa0a86b61d
NurgazyBekmurzaev <nbekmurzaev@gmail.com>
2022-05-14 13:54:53 +0600

    created post

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,9 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('myuser.urls')),
+    path('',include(('post.urls'))),
+    path('api-auth/', include('rest_framework.urls')),
+
 ]
--- post/views.py
+++ post/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .models import Post
+from . import serializers
+from rest_framework import generics
 
-# Create your views here.
+
+
+
+from rest_framework import generics, permissions
+
+from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
